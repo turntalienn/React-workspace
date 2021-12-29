9da71c619ac6ac1bd4d412aa8c6b611361bc7e42
turntalienn <aju369420@gmail.com>
2021-12-29 23:35:19 +0530

    Fixed the context bug

--- food_cart/src/components/Layout/HeaderCartButton.jsx
+++ food_cart/src/components/Layout/HeaderCartButton.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, {useContext} from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.length;
+  
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
